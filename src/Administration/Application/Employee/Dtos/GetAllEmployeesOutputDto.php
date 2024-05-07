9d74ae7ddb6bf90a0238a98fd5e063f808b3e125
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-05-07 11:58:29 -0300

    Criar teste de employee dto

--- src/Administration/Application/Employee/Dtos/GetAllEmployeesOutputDto.php
+++ src/Administration/Application/Employee/Dtos/GetAllEmployeesOutputDto.php
@@ -8,7 +8,7 @@ use Src\Shared\Utils\Notification;
 final class GetAllEmployeesOutputDto
 {
     public function __construct(
-        readonly ?Collection $employee,
+        readonly ?Collection $employees,
         readonly Notification $notification
     ) {
     }
