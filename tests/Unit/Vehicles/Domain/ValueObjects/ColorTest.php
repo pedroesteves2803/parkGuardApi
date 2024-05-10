31724a8f503123f8bc1f5ac56202960e3b6de303
Pedro Esteves <pedroesteves2803@gmail.com>
2024-05-10 01:28:28 -0300

    wip

---
+++ tests/Unit/Vehicles/Domain/ValueObjects/ColorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Src\Vehicles\Domain\ValueObjects\Color;
+
+test('validates instance color', function () {
+    $color = new Color('blue');
+    expect($color)->toBeInstanceOf(Color::class);
+});
+
+it('validates a valid color', function () {
+    $color = new Color('blue');
+    expect($color->value())->toBe('blue');
+});
+
+test('throws an exception for an empty color', function () {
+    $this->expectException(Exception::class);
+    $this->expectExceptionMessage('Color cannot be empty.');
+    new Color('');
+});
+
+test('test color object to string conversion', function () {
+    $color = new Color('blue');
+    expect((string) $color)->toBe('blue');
+});
