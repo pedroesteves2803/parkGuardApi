95ddf3375595ede1851fe22e8d46b2d5bff5e114
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-05-16 12:09:41 -0300

    Atualizar teste de funcionario

--- tests/Unit/Administration/Application/Employee/UpdateEmployeeTest.php
+++ tests/Unit/Administration/Application/Employee/UpdateEmployeeTest.php
@@ -59,6 +59,48 @@ it('can update an existing employee with valid input', function () {
     expect($outputDto->notification->getErrors())->toBeEmpty();
 });
 
+it('can update an existing employee with a valid entry and the same email as the employee sent', function () {
+    $notification = new Notification();
+
+    $employeeId = 1;
+
+    $employee = new Employee(
+        $employeeId,
+        new Name('John Doe'),
+        new Email('john@example.com'),
+        new Password('Password@123'),
+        new Type(1),
+    );
+
+    $this->repositoryMock->shouldReceive('getById')->once()->andReturn($employee);
+
+    $employeeUpdate = new Employee(
+        $employeeId,
+        new Name('Updated Name'),
+        new Email('john@example.com'),
+        new Password('NewPassword@456'),
+        new Type(2),
+    );
+
+    $this->repositoryMock->shouldReceive('update')->once()->andReturn($employeeUpdate);
+
+    $updateEmployee = new UpdateEmployee($this->repositoryMock, $notification);
+
+    $inputDto = new UpdateEmployeeInputDto(
+        $employeeId,
+        'Updated Name',
+        'john@example.com',
+        'NewPassword@456',
+        2,
+    );
+
+    $outputDto = $updateEmployee->execute($inputDto);
+
+    expect($outputDto)->toBeInstanceOf(UpdateEmployeeOutputDto::class);
+    expect($outputDto->employee)->toBe($employeeUpdate);
+    expect($outputDto->notification->getErrors())->toBeEmpty();
+});
+
 it('returns error notification when trying to update an employee with non-existing ID', function () {
     $notification = new Notification();
 
