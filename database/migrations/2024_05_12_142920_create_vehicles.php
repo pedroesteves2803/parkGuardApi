5897e9fb08c2ee9bb5cdf11cc675aa74113e2fea
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-05-16 12:09:12 -0300

    Remover unique da coluna

--- database/migrations/2024_05_12_142920_create_vehicles.php
+++ database/migrations/2024_05_12_142920_create_vehicles.php
@@ -5,7 +5,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('vehicles', function (Blueprint $table) {
@@ -13,13 +15,16 @@ return new class() extends Migration {
             $table->string('manufacturer')->nullable();
             $table->string('color')->nullable();
             $table->string('model')->nullable();
-            $table->string('license_plate');
+            $table->string('license_plate')->unique();
             $table->dateTime('entry_times');
             $table->dateTime('departure_times')->nullable();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('vehicles');
