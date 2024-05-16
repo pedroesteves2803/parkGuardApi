f63f7c47d061895d53c9113897b80ab6a0f11971
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-05-16 12:06:42 -0300

    Atualizar tabela

--- database/migrations/2024_05_12_142920_create_vehicles.php
+++ database/migrations/2024_05_12_142920_create_vehicles.php
@@ -5,9 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('vehicles', function (Blueprint $table) {
@@ -15,16 +13,13 @@ return new class() extends Migration {
             $table->string('manufacturer')->nullable();
             $table->string('color')->nullable();
             $table->string('model')->nullable();
-            $table->string('license_plate')->unique();
+            $table->string('license_plate');
             $table->dateTime('entry_times');
             $table->dateTime('departure_times')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('vehicles');
