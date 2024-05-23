aa7328c0b394971495257e82083b5241bc67ed10
Pedro Esteves <pedroesteves2803@gmail.com>
2024-05-23 00:11:02 -0300

    wip

---
+++ tests/Unit/Vehicles/Application/Vehicle/Dtos/GetVehicleInputDtoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Src\Vehicles\Application\Vehicle\Dtos\GetVehicleInputDto;
+
+it('can create an instance of GetVehicleInputDto with a valid ID', function () {
+    $id = 1;
+
+    $inputDto = new GetVehicleInputDto($id);
+
+    expect($inputDto)->toBeInstanceOf(GetVehicleInputDto::class);
+    expect($inputDto->id)->toBe($id);
+});
---
+++ tests/Unit/Vehicles/Application/Vehicle/Dtos/GetVehicleOutputDtoTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use Src\Administration\Application\Employee\Dtos\GetEmployeeByIdOutputDto;
+use Src\Shared\Utils\Notification;
+use Src\Vehicles\Application\Vehicle\Dtos\GetVehicleOutputDto;
+use Src\Vehicles\Domain\Entities\Vehicle;
+use Src\Vehicles\Domain\ValueObjects\Color;
+use Src\Vehicles\Domain\ValueObjects\EntryTimes;
+use Src\Vehicles\Domain\ValueObjects\LicensePlate;
+use Src\Vehicles\Domain\ValueObjects\Manufacturer;
+use Src\Vehicles\Domain\ValueObjects\Model;
+
+it('can create an instance of GetVehicleOutputDto with a valid vehicle', function () {
+    $notification = new Notification();
+
+    $vehicle = new Vehicle(
+        1,
+        new Manufacturer('Toyota'),
+        new Color('Azul'),
+        new Model('Corolla'),
+        new LicensePlate('ABC-1234'),
+        new EntryTimes(new DateTime('2024-05-12 08:00:00')),
+        null
+    );
+
+    $outputDto = new GetVehicleOutputDto($vehicle, $notification);
+
+    expect($outputDto)->toBeInstanceOf(GetVehicleOutputDto::class);
+    expect($outputDto->vehicle)->toBe($vehicle);
+    expect($outputDto->notification)->toBe($notification);
+    expect($outputDto->notification->getErrors())->toBe([]);
+});
+
+it('can create an instance of GetVehicleOutputDto with null employee and error notifications', function () {
+    $notification = new Notification();
+
+    $notification->addError([
+        'context' => 'test_error',
+        'message' => 'test',
+    ]);
+
+    $outputDto = new GetVehicleOutputDto(null, $notification);
+
+    expect($outputDto)->toBeInstanceOf(GetVehicleOutputDto::class);
+    expect($outputDto->vehicle)->toBeNull();
+    expect($outputDto->notification->getErrors())->toBe([
+        [
+            'context' => 'test_error',
+            'message' => 'test',
+        ],
+    ]);
+});
