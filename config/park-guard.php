aff6b790247263099e6d5684159a23fd821380d7
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-09-03 18:48:35 -0300

    Alterar config.

--- config/park-guard.php
+++ config/park-guard.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_url' => env('PARK_GUARD_API_URL', null),
+    'api_url' => env('API_URL', null),
     'api_token' => env('API_TOKEN', null),
     'api_device_token' => env('DEVICE_TOKEN', null),
     'api_verify_ssl' => env('VERIFY_SSL', false),
